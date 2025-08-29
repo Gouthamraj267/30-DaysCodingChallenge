c49855d3c136682908a5de7c892cf13ec94aa1ad
Gouthamraj267 <22311a04j1@ece.sreenidhi.edu.in>
2025-08-29 19:42:05 +0530

    Day 29

---
+++ BinaryTreeBFS.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode>queue=new LinkedList<>();
+        List<List<Integer>>list=new ArrayList<>();
+        if(root==null){
+            return list;
+        }
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            List<Integer>current=new ArrayList<>();
+            int len=queue.size();
+            for(int i=0;i<len;i++){
+                if(queue.peek().left!=null){
+                    queue.offer(queue.peek().left);
+                }
+                if(queue.peek().right!=null){
+                    queue.offer(queue.peek().right);
+                }
+                current.add(queue.poll().val);
+            }
+            list.add(current);
+        }
+        return list;
+    }
+}
